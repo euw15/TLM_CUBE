5a2302c4e49508595df5358948106b28a6224d24
Edward <edwarduw@gmail.com>
2018-07-24 03:52:44 -0600

    Ahora solo hace 1 respuesta

--- Examples/non_blocking_edward/non_blocking.cpp
+++ Examples/non_blocking_edward/non_blocking.cpp
@@ -34,7 +34,7 @@ struct Initiator: sc_module
   Socket used to send the requests 
   */
   tlm_utils::simple_initiator_socket<Initiator> initSocket; 
-   
+  int data;   
 
   SC_CTOR(Initiator)   
   : initSocket("initSocket")   
@@ -56,40 +56,24 @@ struct Initiator: sc_module
      
     /*Set the request information in the payload*/
     tlm::tlm_phase phase = tlm::BEGIN_REQ;   
-    sc_time delay = sc_time(10, SC_NS);   
+    sc_time delay = sc_time(20, SC_NS);   
     tlm::tlm_command cmd = static_cast<tlm::tlm_command>(rand() % 2);   
     if (cmd == tlm::TLM_WRITE_COMMAND) data = 0xFF000000 | 0;   
     trans.set_command( cmd );   
     trans.set_address( rand() % 0xFF );   
     trans.set_data_ptr( reinterpret_cast<unsigned char*>(&data) );   
-    trans.set_data_length( 4 );   
+    trans.set_data_length( sizeof(int));   
  
     /*Delay for BEGIN_REQ*/
     wait(10, SC_NS);
     /*Send the request to the next socket*/
     cout << name() << " BEGIN_REQ SENT" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;  
-    tlm::tlm_sync_enum status = initSocket->nb_transport_fw( trans, phase, delay );   
- 
-    /*The requeset was accepted therefor we send the END REQ to the memory*/
-    if(status == tlm::TLM_ACCEPTED)
-    {
-      /*Delay for END_REQ*/
-      wait(10, SC_NS);
-      cout << name() << " END_REQ SENT" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
-      /* Expect response on the backward path */  
-      phase = tlm::END_REQ; 
-      status = initSocket->nb_transport_fw( trans, phase, delay ); 
-    }
-    else
-    {
-      if (trans.is_response_error() )   
-        SC_REPORT_ERROR("TLM2", "Response error from nb_transport_fw");   
-      cout<<name() << " UNHANDLE STATUS";
-    }
-      /*Delay between RD/WR request*/
-      wait(100, SC_NS);
+    tlm::tlm_sync_enum status = initSocket->nb_transport_fw( trans, phase, delay );
+    
+    /*Delay between RD/WR request*/
+    wait(100, SC_NS);
       
-      id_extension->transaction_id++; 
+    id_extension->transaction_id++; 
   }   
    
   // *********************************************   
@@ -105,37 +89,20 @@ struct Initiator: sc_module
     ID_extension* id_extension = new ID_extension;
     trans.get_extension( id_extension ); 
     
-    if (phase == tlm::END_RESP) {  
-           
-      //Delay for TLM_COMPLETE
-      wait(delay);
-      
-      cout << name() << " END_RESP RECEIVED" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
-            
-      return tlm::TLM_COMPLETED;
-        
-    }
-    
     if (phase == tlm::BEGIN_RESP) {
-                              
       // Initiator obliged to check response status   
       if (trans.is_response_error() )   
         SC_REPORT_ERROR("TLM2", "Response error from nb_transport");   
             
+      //Delay
+      wait(delay);
+      cout << name () << " BEGIN_RESP RECEIVED" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
       cout << "INIT trans/bw = { " << (cmd ? 'W' : 'R') << ", " << hex << adr   
            << " } , data = " << hex << data << " at time " << sc_time_stamp()   
            << ", delay = " << delay << endl;
-      
-      //Delay
-      wait(delay);
-      
-      cout << name () << " BEGIN_RESP RECEIVED" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
-      
       return tlm::TLM_ACCEPTED;   
     }   
   }   
-  // Internal data buffer used by initiator with generic payload   
-  int data;   
 };   
      
    
@@ -149,13 +116,18 @@ struct Router: sc_module
   tlm_utils::simple_target_socket<Router> targetSocket;
    
   /*Event used to notify the thread it has to iterate*/
-  sc_event  e1;
+  sc_event  fordwardEvent;
+  sc_event  backwardEvent;
 
   /*Referance to trans recive in FW method*/
   tlm::tlm_generic_payload* trans_pending;   
   tlm::tlm_phase phase_pending;   
   sc_time delay_pending;
-  int data;
+
+  /*Referance to trans recive in FW method*/
+  tlm::tlm_generic_payload* trans_bw;   
+  tlm::tlm_phase phase_bw;   
+  sc_time delay_bw;
 
 
   SC_CTOR(Router)   
@@ -164,26 +136,16 @@ struct Router: sc_module
     // Register callbacks for incoming interface method calls
     initSocket.register_nb_transport_bw(this, &Router::nb_transport_bw);
     targetSocket.register_nb_transport_fw(this, &Router::nb_transport_fw);
-    SC_THREAD(thread_process);  
+    SC_THREAD(fordwardThread);  
+    SC_THREAD(backwardThread);  
   }  
 
-  virtual tlm::tlm_sync_enum nb_transport_fw( tlm::tlm_generic_payload& trans,
-                                              tlm::tlm_phase& phase, sc_time& delay )
+  virtual tlm::tlm_sync_enum nb_transport_fw( tlm::tlm_generic_payload& trans,tlm::tlm_phase& phase, sc_time& delay )
   {
-    sc_dt::uint64    adr = trans.get_address();
-    unsigned int     len = trans.get_data_length();
     unsigned char*   byt = trans.get_byte_enable_ptr();
-    unsigned int     wid = trans.get_streaming_width();
     ID_extension* id_extension = new ID_extension;
     trans.get_extension( id_extension ); 
     
-    if(phase == tlm::END_REQ){
-      
-      wait(delay);
-      cout << name() << " END_REQ RECEIVED" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
-      
-      return tlm::TLM_COMPLETED;
-    }
     if(phase == tlm::BEGIN_REQ){
       // Obliged to check the transaction attributes for unsupported features
       // and to generate the appropriate error response
@@ -191,75 +153,53 @@ struct Router: sc_module
         trans.set_response_status( tlm::TLM_BYTE_ENABLE_ERROR_RESPONSE );
         return tlm::TLM_COMPLETED;
       }
-      //if (len > 4 || wid < len) {
-      //  trans.set_response_status( tlm::TLM_BURST_ERROR_RESPONSE );
-      //  return tlm::TLM_COMPLETED;
-      //}
-
+      
       // Now queue the transaction until the annotated time has elapsed
       trans_pending=&trans;
       phase_pending=phase;
       delay_pending=delay;
 
-      e1.notify();
+      fordwardEvent.notify();
       
       //Delay
       wait(delay);
       
       cout << name() << " BEGIN_REQ RECEIVED" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;      
       
-      return tlm::TLM_ACCEPTED;
+      return tlm::TLM_COMPLETED;
     }  
   }
 
-  virtual tlm::tlm_sync_enum nb_transport_bw( tlm::tlm_generic_payload& trans,   
-                                           tlm::tlm_phase& phase, sc_time& delay )   
-  {   
-    tlm::tlm_command cmd = trans.get_command();   
-    sc_dt::uint64    adr = trans.get_address();   
-    
+  virtual tlm::tlm_sync_enum nb_transport_bw( tlm::tlm_generic_payload& trans,tlm::tlm_phase& phase, sc_time& delay )   
+  {     
     ID_extension* id_extension = new ID_extension;
     trans.get_extension( id_extension ); 
-    
-    if (phase == tlm::END_RESP) {  
-           
-      //Delay for TLM_COMPLETE
-      tlm::tlm_sync_enum status;
-      phase = tlm::END_RESP; 
-      status = targetSocket->nb_transport_bw( trans, phase, delay );
-      wait(delay);
-      
-      cout << name() << " END_RESP RECEIVED" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
-      
-      return tlm::TLM_COMPLETED;
-        
-    }
-    
+  
+    /*Check the resp was done*/
     if (phase == tlm::BEGIN_RESP) {
                               
-      // Initiator obliged to check response status   
+      /*Initiator obliged to check response status*/   
       if (trans.is_response_error() )   
         SC_REPORT_ERROR("TLM2", "Response error from nb_transport");   
       
-      tlm::tlm_sync_enum status;
-      phase = tlm::BEGIN_RESP;   
-      status = targetSocket->nb_transport_bw( trans, phase, delay );         
-      
-      
-      //Delay
-      wait(delay);
-      
+      /*Delegates the problem to the thread*/
+      trans_bw=&trans;
+      phase_bw=phase;
+      delay_bw=delay;
+             
+      backwardEvent.notify();
+    
       cout << name () << " BEGIN_RESP RECEIVED" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
       
       return tlm::TLM_ACCEPTED;   
     }   
   } 
   
-  void thread_process()  
+  void fordwardThread()  
   {   
     while (true) {
       // Wait for an event to pop out of the back end of the queue   
-      wait(e1);  
+      wait(fordwardEvent);  
       ID_extension* id_extension = new ID_extension;
       trans_pending->get_extension( id_extension ); 
       tlm::tlm_sync_enum status; 
@@ -270,30 +210,6 @@ struct Router: sc_module
       
       delay_pending=  sc_time(10, SC_NS);
       
-     
-      
-      switch (status)   {   
-      case tlm::TLM_ACCEPTED:   
-        
-        //Delay for END_REQ
-        cout << name() << " END_REQ SENT" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
-        // Expect response on the backward path  
-        phase_pending = tlm::END_REQ; 
-        status = initSocket->nb_transport_fw(*trans_pending, phase_pending, delay_pending);  // Non-blocking transport call
-        break;   
-   
-      case tlm::TLM_UPDATED:   
-      case tlm::TLM_COMPLETED:   
-   
-        // Initiator obliged to check response status   
-        if (trans_pending->is_response_error() )   
-          SC_REPORT_ERROR("TLM2", "Response error from nb_transport_fw");   
-         cout<<phase_pending<<"->TERMINE thread_Router\n";
-        //cout << "trans/fw = { " << (trans_pending->cmd ? 'W' : 'R') << ", " << hex << i << " } , data = "   
-        //     << hex << data << " at time " << sc_time_stamp() << ", delay = " << delay << endl;   
-        break;   
-      }
-
       //Delay between RD/WR request
       wait(100, SC_NS);
       
@@ -302,6 +218,37 @@ struct Router: sc_module
     }   
   } 
 
+  void backwardThread()
+  {
+    while(true){
+
+      wait(backwardEvent);
+
+      tlm::tlm_command cmd = trans_bw->get_command();   
+      sc_dt::uint64    adr = trans_bw->get_address();   
+      
+      ID_extension* id_extension = new ID_extension;
+      trans_bw->get_extension( id_extension ); 
+  
+    
+      if (phase_bw == tlm::BEGIN_RESP) {
+                                
+        // Initiator obliged to check response status   
+        if (trans_bw->is_response_error() )   
+          SC_REPORT_ERROR("TLM2", "Response error from nb_transport");   
+        
+        phase_bw = tlm::BEGIN_RESP;   
+        targetSocket->nb_transport_bw( *trans_bw, phase_bw, delay_bw );         
+        
+        
+        //Delay
+        wait(delay_bw);
+        
+        cout << name () << " BEGIN_RESP RECEIVED" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
+        
+      }   
+    }
+  }
 
 };   
 // Target module representing a simple memory   
@@ -341,14 +288,7 @@ struct Memory: sc_module
     ID_extension* id_extension = new ID_extension;
     trans.get_extension( id_extension ); 
     
-    if(phase == tlm::END_REQ){
-      
-      wait(delay);
-      
-      cout << name() << " END_REQ RECEIVED" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;
-      
-      return tlm::TLM_COMPLETED;
-    }
+  
     if(phase == tlm::BEGIN_REQ){
       // Obliged to check the transaction attributes for unsupported features
       // and to generate the appropriate error response
@@ -356,11 +296,7 @@ struct Memory: sc_module
         trans.set_response_status( tlm::TLM_BYTE_ENABLE_ERROR_RESPONSE );
         return tlm::TLM_COMPLETED;
       }
-      //if (len > 4 || wid < len) {
-      //  trans.set_response_status( tlm::TLM_BURST_ERROR_RESPONSE );
-      //  return tlm::TLM_COMPLETED;
-      //}
-
+     
       // Now queue the transaction until the annotated time has elapsed
       trans_pending=&trans;
       phase_pending=phase;
@@ -442,10 +378,10 @@ struct Memory: sc_module
           
           wait(10, SC_NS);
           
-          cout << name() << " END_RESP SENT" << " TRANS ID " << id_extension->transaction_id <<  " at time " << sc_time_stamp() << endl;
+          //cout << name() << " END_RESP SENT" << " TRANS ID " << id_extension->transaction_id <<  " at time " << sc_time_stamp() << endl;
           // Expect response on the backward path  
           phase = tlm::END_RESP; 
-          targetSocket->nb_transport_bw( *trans_pending, phase, delay_pending );  // Non-blocking transport call
+          //targetSocket->nb_transport_bw( *trans_pending, phase, delay_pending );  // Non-blocking transport call
         //break;   
         
     }   
