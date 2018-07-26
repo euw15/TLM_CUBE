735475b7abdbc012b2b56bdefd585ccb76f6d851
Edward <edwarduw@gmail.com>
2018-07-25 21:26:32 -0600

    Added Cube conections, missimg backwards messages

--- Examples/non_blocking_edward/non_blocking.cpp
+++ Examples/non_blocking_edward/non_blocking.cpp
@@ -23,7 +23,21 @@ struct ID_extension: tlm::tlm_extension<ID_extension> {
   unsigned int transaction_id;
 };
 
+/*Struct use for the Queue of messages and calbacks*/
+struct MessageInfo
+{
+  tlm::tlm_generic_payload* transaction;
+  tlm::tlm_phase phase;
+  sc_time delay;
+};
 
+/*Enuma that handle the directions the reqquest can do*/
+enum class RequestDirection{
+  MOVE_X,
+  MOVE_Y,
+  MOVE_Z,
+  NO_MOVE
+};
 /*
 Initiator start sending messages to diferents nodes.
 Has a thread processs which creates the transactions
@@ -33,52 +47,20 @@ struct Initiator: sc_module
   /*
   Socket used to send the requests 
   */
-  tlm_utils::simple_initiator_socket<Initiator> initSocket; 
+  tlm_utils::simple_initiator_socket<Initiator> xInitSocket; 
+  tlm_utils::simple_initiator_socket<Initiator> yInitSocket; 
+  tlm_utils::simple_initiator_socket<Initiator> zInitSocket; 
   int data;   
 
   SC_CTOR(Initiator)   
-  : initSocket("initSocket")   
+  : xInitSocket("xInitSocket"),yInitSocket("yInitSocket"),zInitSocket("zInitSocket")   
   {   
     /* Register callbacks for incoming messages*/
-    initSocket.register_nb_transport_bw(this, &Initiator::nb_transport_bw);
-    
-    SC_THREAD(createRequestsThread);   
-  }   
-   
-  void createRequestsThread()   
-  {   
-    /*TML Payload is the struct use to send a request*/
-    tlm::tlm_generic_payload trans;
-
-    /*Sort of the id of the transaction*/
-    ID_extension* id_extension = new ID_extension;
-    trans.set_extension( id_extension ); 
+    xInitSocket.register_nb_transport_bw(this, &Initiator::nb_transport_bw);
+    yInitSocket.register_nb_transport_bw(this, &Initiator::nb_transport_bw);
+    zInitSocket.register_nb_transport_bw(this, &Initiator::nb_transport_bw);
      
-    /*Set the request information in the payload*/
-    tlm::tlm_phase phase = tlm::BEGIN_REQ;   
-    sc_time delay = sc_time(20, SC_NS);   
-    tlm::tlm_command cmd = static_cast<tlm::tlm_command>(rand() % 2);   
-    if (cmd == tlm::TLM_WRITE_COMMAND) data = 0xFF000000 | 0;   
-    trans.set_command( cmd );   
-    trans.set_address( rand() % 0xFF );   
-    trans.set_data_ptr( reinterpret_cast<unsigned char*>(&data) );   
-    trans.set_data_length( sizeof(int));   
- 
-    /*Delay for BEGIN_REQ*/
-    wait(10, SC_NS);
-    /*Send the request to the next socket*/
-    cout << name() << " BEGIN_REQ SENT" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;  
-    tlm::tlm_sync_enum status = initSocket->nb_transport_fw( trans, phase, delay );
-    
-    /*Delay between RD/WR request*/
-    wait(100, SC_NS);
-      
-    id_extension->transaction_id++; 
   }   
-   
-  // *********************************************   
-  // TLM2 backward path non-blocking transport method   
-  // *********************************************   
    
   virtual tlm::tlm_sync_enum nb_transport_bw( tlm::tlm_generic_payload& trans,   
                                            tlm::tlm_phase& phase, sc_time& delay )   
@@ -105,12 +87,7 @@ struct Initiator: sc_module
   }   
 };   
      
-struct MessageInfo
-{
-  tlm::tlm_generic_payload* transaction;
-  tlm::tlm_phase phase;
-  sc_time delay;
-};
+
 /*
 The Router module only sends request
 */
@@ -119,28 +96,45 @@ struct Router: sc_module
   /*Queue with the transtions pending*/
   std::queue<MessageInfo> fordwardQueue;
   std::queue<MessageInfo> backwardQueue;
+
   /*Init and target socket use to send and recibe messages*/
-  tlm_utils::simple_initiator_socket<Router> initSocket; 
-  tlm_utils::simple_target_socket<Router> targetSocket;
-   
+  tlm_utils::simple_initiator_socket<Router>  xInitSocket;
+  tlm_utils::simple_initiator_socket<Router>  yInitSocket; 
+  tlm_utils::simple_initiator_socket<Router>  zInitSocket; 
+  tlm_utils::simple_target_socket<Router>     xTargetSocket;
+  tlm_utils::simple_target_socket<Router>     yTargetSocket;
+  tlm_utils::simple_target_socket<Router>     zTargetSocket;
+  
   /*Event used to notify the thread it has to iterate*/
   sc_event  fordwardEvent;
   sc_event  backwardEvent;
 
   /*Referance to trans recive in FW method*/
   tlm::tlm_generic_payload* trans_bw;   
-  tlm::tlm_phase phase_bw;   
-  sc_time delay_bw;
+  tlm::tlm_phase            phase_bw;   
+  sc_time                   delay_bw;
 
+  /*Default ID*/
+  short m_id = -1;
+
+  /*For init router*/
+  int data;
 
   SC_CTOR(Router)   
-  : initSocket("initSocket"),targetSocket("targetSocket")  // Construct and name socket   
+  : xInitSocket("xInitSocket"),xTargetSocket("xTargetSocket"),
+    yInitSocket("yInitSocket"),yTargetSocket("yTargetSocket"),
+    zInitSocket("zInitSocket"),zTargetSocket("zTargetSocket")
   {   
     // Register callbacks for incoming interface method calls
-    initSocket.register_nb_transport_bw(this, &Router::nb_transport_bw);
-    targetSocket.register_nb_transport_fw(this, &Router::nb_transport_fw);
+    xInitSocket.register_nb_transport_bw(this, &Router::nb_transport_bw);
+    xTargetSocket.register_nb_transport_fw(this, &Router::nb_transport_fw);
+    yInitSocket.register_nb_transport_bw(this, &Router::nb_transport_bw);
+    yTargetSocket.register_nb_transport_fw(this, &Router::nb_transport_fw);
+    zInitSocket.register_nb_transport_bw(this, &Router::nb_transport_bw);
+    zTargetSocket.register_nb_transport_fw(this, &Router::nb_transport_fw);
     SC_THREAD(fordwardThread);  
-    SC_THREAD(backwardThread);  
+    SC_THREAD(backwardThread);
+    SC_THREAD(createRequestsThread);
   }  
 
   virtual tlm::tlm_sync_enum nb_transport_fw( tlm::tlm_generic_payload& trans,tlm::tlm_phase& phase, sc_time& delay )
@@ -200,8 +194,22 @@ struct Router: sc_module
         MessageInfo pendingMessage = fordwardQueue.front();
         ID_extension* id_extension = new ID_extension;
         pendingMessage.transaction->get_extension( id_extension );
-        initSocket->nb_transport_fw(*pendingMessage.transaction, pendingMessage.phase, pendingMessage.delay);
-        
+        pendingMessage.delay = sc_time(10, SC_NS);
+
+        /*Send the request to the corresponding socket*/
+        RequestDirection nextStep = calculateForwardRoute(m_id);
+        if(nextStep == RequestDirection::MOVE_X){
+          xInitSocket->nb_transport_fw(*pendingMessage.transaction, pendingMessage.phase, pendingMessage.delay);
+        }
+        else if(nextStep == RequestDirection::MOVE_Y){
+          yInitSocket->nb_transport_fw(*pendingMessage.transaction, pendingMessage.phase, pendingMessage.delay);
+        }
+        else if(nextStep == RequestDirection::MOVE_Z){
+          zInitSocket->nb_transport_fw(*pendingMessage.transaction, pendingMessage.phase, pendingMessage.delay);
+        }
+        else{
+          cout<<"SOMETHING IS WRONG";
+        }
         /*Delay between RD/WR request*/
         wait(pendingMessage.delay);
         
@@ -232,9 +240,12 @@ struct Router: sc_module
           if (backwardMessage.transaction->is_response_error() )   
             SC_REPORT_ERROR("TLM2", "Response error from nb_transport");   
           
-          backwardMessage.phase = tlm::BEGIN_RESP;   
-          targetSocket->nb_transport_bw( *backwardMessage.transaction, backwardMessage.phase, backwardMessage.delay);         
-          
+          backwardMessage.phase = tlm::BEGIN_RESP;
+          backwardMessage.delay = sc_time(10, SC_NS);
+
+          xTargetSocket->nb_transport_bw( *backwardMessage.transaction, backwardMessage.phase, backwardMessage.delay);         
+          yTargetSocket->nb_transport_bw( *backwardMessage.transaction, backwardMessage.phase, backwardMessage.delay);
+          zTargetSocket->nb_transport_bw( *backwardMessage.transaction, backwardMessage.phase, backwardMessage.delay);
           //Delay
           wait(backwardMessage.delay);
           
@@ -245,33 +256,100 @@ struct Router: sc_module
     }
   }
 
-};   
-// Target module representing a simple memory   
+  void createRequestsThread()   
+  {   
+    if(m_id == 0){
+      /*TML Payload is the struct use to send a request*/
+      tlm::tlm_generic_payload trans;
+
+      /*Sort of the id of the transaction*/
+      ID_extension* id_extension = new ID_extension;
+      trans.set_extension( id_extension ); 
+       
+      /*Set the request information in the payload*/
+      tlm::tlm_phase phase = tlm::BEGIN_REQ;   
+      sc_time delay = sc_time(10, SC_NS);   
+      tlm::tlm_command cmd = static_cast<tlm::tlm_command>(rand() % 2);   
+      if (cmd == tlm::TLM_WRITE_COMMAND) data = 0xFF000000 | 0;   
+      trans.set_command( cmd );   
+      trans.set_address( rand() % 0xFF );   
+      trans.set_data_ptr( reinterpret_cast<unsigned char*>(&data) );   
+      trans.set_data_length( sizeof(int));   
    
+      /*Delay for BEGIN_REQ*/
+      wait(10, SC_NS);
+      /*Send the request to the next socket*/
+      cout << name() << " BEGIN_REQ SENT" << " TRANS ID " << id_extension->transaction_id << " at time " << sc_time_stamp() << endl;  
+      tlm::tlm_sync_enum status = xInitSocket->nb_transport_fw( trans, phase, delay );
+      
+      /*Delay between RD/WR request*/
+      wait(100, SC_NS);
+        
+      id_extension->transaction_id++; 
+    }
+  }  
+
+  RequestDirection calculateForwardRoute(short id){
+    switch(id){
+      case 0:
+        return RequestDirection::MOVE_X;
+      case 1:
+        return RequestDirection::MOVE_Y;
+      case 2:
+        return RequestDirection::MOVE_X;
+      case 3:
+        return RequestDirection::MOVE_Y;
+      case 4:
+        return RequestDirection::MOVE_Z;
+      case 5:
+        return RequestDirection::MOVE_Z;
+      case 6:
+        return RequestDirection::MOVE_X;
+      case 7:
+        return RequestDirection::MOVE_X;
+      default:
+        return RequestDirection::NO_MOVE;
+    }
+  }
+  
+  void setId(short id){
+     m_id = id;
+  };
+
+};   
+
+/* Target memory*/  
 struct Memory: sc_module   
 {   
-  // TLM-2 socket, defaults to 32-bits wide, base protocol
-  tlm_utils::simple_target_socket<Memory> targetSocket;
-  
+  /*Multiple targets but only target x is used*/
+  tlm_utils::simple_target_socket<Memory> xTargetSocket;
+  tlm_utils::simple_target_socket<Memory> yTargetSocket;
+  tlm_utils::simple_target_socket<Memory> zTargetSocket;
+
+  /*Message request queue*/
+  std::queue<MessageInfo> incomingRequest;
+
+  /*Wait times*/
   enum { SIZE = 256 };   
   const sc_time LATENCY;   
-   
+  
+  int mem[SIZE];   
+  sc_event  requestEvent;
+
   SC_CTOR(Memory)   
-  : targetSocket("targetSocket"), LATENCY(10, SC_NS)   
+  : xTargetSocket("xTargetSocket"),yTargetSocket("yTargetSocket"),zTargetSocket("zTargetSocket"), LATENCY(10, SC_NS)   
   {   
-    // Register callbacks for incoming interface method calls
-    targetSocket.register_nb_transport_fw(this, &Memory::nb_transport_fw);
-    //targetSocket.register_nb_transport_bw(this, &Memory::nb_transport_bw);
-   
+    xTargetSocket.register_nb_transport_fw(this, &Memory::nb_transport_fw);
+    yTargetSocket.register_nb_transport_fw(this, &Memory::nb_transport_fw);
+    zTargetSocket.register_nb_transport_fw(this, &Memory::nb_transport_fw);
+
     // Initialize memory with random data   
     for (int i = 0; i < SIZE; i++)   
       mem[i] = 0xAA000000 | (rand() % 256);   
    
-    SC_THREAD(thread_process);   
+    SC_THREAD(memoryRespondRequest);   
   }   
-   
-  // TLM2 non-blocking transport method 
-  
+
   virtual tlm::tlm_sync_enum nb_transport_fw( tlm::tlm_generic_payload& trans,
                                               tlm::tlm_phase& phase, sc_time& delay )
   {
@@ -287,13 +365,10 @@ struct Memory: sc_module
         trans.set_response_status( tlm::TLM_BYTE_ENABLE_ERROR_RESPONSE );
         return tlm::TLM_COMPLETED;
       }
-     
-      // Now queue the transaction until the annotated time has elapsed
-      trans_pending=&trans;
-      phase_pending=phase;
-      delay_pending=delay;
-
-      e1.notify();
+    
+      MessageInfo pendingMessage {&trans,phase,delay};
+      incomingRequest.push(pendingMessage);
+      requestEvent.notify();
       
       //Delay
       wait(delay);
@@ -303,35 +378,25 @@ struct Memory: sc_module
       return tlm::TLM_ACCEPTED;
     }  
   }
-  
-  // *********************************************   
-  // Thread to call nb_transport on backward path   
-  // ********************************************* 
-   
-  void thread_process()  
+  /*Thread handling memory requests*/
+  void memoryRespondRequest()  
   {   
     while (true) {
       // Wait for an event to pop out of the back end of the queue   
-      wait(e1); 
-      //printf("ACCESING MEMORY\n");
+      wait(requestEvent); 
       
-      //tlm::tlm_generic_payload* trans_ptr;   
-      tlm::tlm_phase phase;   
-      
+      MessageInfo request = incomingRequest.front();     
+    
+
       ID_extension* id_extension = new ID_extension;
-      trans_pending->get_extension( id_extension ); 
+      request.transaction->get_extension( id_extension ); 
       
-      tlm::tlm_command cmd = trans_pending->get_command();   
-      sc_dt::uint64    adr = trans_pending->get_address() / 4;   
-      unsigned char*   ptr = trans_pending->get_data_ptr();   
-      unsigned int     len = trans_pending->get_data_length();   
-      unsigned char*   byt = trans_pending->get_byte_enable_ptr();   
-      unsigned int     wid = trans_pending->get_streaming_width();   
-   
-      // Obliged to check address range and check for unsupported features,   
-      //   i.e. byte enables, streaming, and bursts   
-      // Can ignore DMI hint and extensions   
-      // Using the SystemC report handler is an acceptable way of signalling an error   
+      tlm::tlm_command cmd = request.transaction->get_command();   
+      sc_dt::uint64    adr = request.transaction->get_address() / 4;   
+      unsigned char*   ptr = request.transaction->get_data_ptr();   
+      unsigned int     len = request.transaction->get_data_length();   
+      unsigned char*   byt = request.transaction->get_byte_enable_ptr();   
+      unsigned int     wid = request.transaction->get_streaming_width();   
      
       if (adr >= sc_dt::uint64(SIZE) || byt != 0 || wid != 0 || len > 4)   
         SC_REPORT_ERROR("TLM2", "Target does not support given generic payload transaction");   
@@ -343,71 +408,100 @@ struct Memory: sc_module
         memcpy(&mem[adr], ptr, len);   
 
       // Obliged to set response status to indicate successful completion   
-      trans_pending->set_response_status( tlm::TLM_OK_RESPONSE );  
+      request.transaction->set_response_status( tlm::TLM_OK_RESPONSE );  
       
       wait(20, SC_NS);
       
-      delay_pending= (rand() % 4) * sc_time(10, SC_NS);
+      request.delay = sc_time(10, SC_NS);
       
       cout << name() << " BEGIN_RESP SENT" << " TRANS ID " << id_extension->transaction_id <<  " at time " << sc_time_stamp() << endl;
       
       // Call on backward path to complete the transaction
       tlm::tlm_sync_enum status;
-        phase = tlm::BEGIN_RESP;   
-      status = targetSocket->nb_transport_bw( *trans_pending, phase, delay_pending );   
-   
-        // The target gets a final chance to read or update the transaction object at this point.   
-        // Once this process yields, the target must assume that the transaction object   
-        // will be deleted by the initiator   
-   
-      // Check value returned from nb_transport   
-   
-      switch (status)   
-        
-      //case tlm::TLM_REJECTED:   
-        case tlm::TLM_ACCEPTED:   
-          
-          wait(10, SC_NS);
-          
-          //cout << name() << " END_RESP SENT" << " TRANS ID " << id_extension->transaction_id <<  " at time " << sc_time_stamp() << endl;
-          // Expect response on the backward path  
-          phase = tlm::END_RESP; 
-          //targetSocket->nb_transport_bw( *trans_pending, phase, delay_pending );  // Non-blocking transport call
-        //break;   
-        
+        request.phase = tlm::BEGIN_RESP;   
+      status = xTargetSocket->nb_transport_bw( *request.transaction, request.phase, request.delay );   
     }   
-  } 
-   
-  int mem[SIZE];   
-  sc_event  e1;
-  tlm::tlm_generic_payload* trans_pending;   
-  tlm::tlm_phase phase_pending;   
-  sc_time delay_pending;
-    
+  }   
+
+  
 };   
    
    
 SC_MODULE(Top)   
-{   
-  Initiator *initiator;
+{  
+  Router    *router0;
   Router    *router1;
-  Router    *router2;    
+  Router    *router2;  
+  Router    *router3;
+  Router    *router4;  
+  Router    *router5;
+  Router    *router6;  
+  Router    *router7;   
   Memory    *memory;   
-   
+  Initiator *dummy;
+
   SC_CTOR(Top)   
   {   
-    // Instantiate components   
-    initiator = new Initiator("initiator");  
+    /*Init routers and memory*/ 
+    router0    = new Router("router0");
     router1    = new Router("router1");
     router2    = new Router("router2");
-    memory    = new Memory   ("memory");   
+    router3    = new Router("router3");
+    router4    = new Router("router4");
+    router5    = new Router("router5");
+    router6    = new Router("router6");
+    router7    = new Router("router7");
+    memory     = new Memory("memory");   
+    dummy      = new Initiator("dummy");
+
+    /*Set Routers numbers IDs*/
+    router0->setId(0);
+    router1->setId(1);
+    router2->setId(2);
+    router3->setId(3);
+    router4->setId(4);
+    router5->setId(5);
+    router6->setId(6);
+    router7->setId(7);
+
+    /*Creates the cub interconation*/
+    router0->xInitSocket(router1->xTargetSocket);
+    router0->yInitSocket(router4->yTargetSocket);
+    router0->zInitSocket(router2->zTargetSocket);
+
+    router1->xInitSocket(router0->xTargetSocket);
+    router1->yInitSocket(router5->yTargetSocket);
+    router1->zInitSocket(router3->zTargetSocket);
+
+    router2->xInitSocket(router3->xTargetSocket);
+    router2->yInitSocket(router6->yTargetSocket);
+    router2->zInitSocket(router0->zTargetSocket);
+
+    router3->xInitSocket(router2->xTargetSocket);
+    router3->yInitSocket(router7->yTargetSocket);
+    router3->zInitSocket(router1->zTargetSocket);
+
+    router4->xInitSocket(router5->xTargetSocket);
+    router4->yInitSocket(router0->yTargetSocket);
+    router4->zInitSocket(router6->zTargetSocket);
    
-    // One initiator is bound directly to one target with no intervening bus   
-   
-    // Bind initiator targetSocket to target socket   
-    initiator->initSocket.bind(router1->targetSocket);
-    router1->initSocket.bind(router2->targetSocket);  
-    router2->initSocket.bind(memory->targetSocket);
+    router5->xInitSocket(router4->xTargetSocket);
+    router5->yInitSocket(router1->yTargetSocket);
+    router5->zInitSocket(router7->zTargetSocket);
+
+    router6->xInitSocket(router7->xTargetSocket);
+    router6->yInitSocket(router2->yTargetSocket);
+    router6->zInitSocket(router4->zTargetSocket);
+
+    router7->xInitSocket(memory->xTargetSocket);
+    router7->yInitSocket(memory->yTargetSocket);
+    router7->zInitSocket(memory->zTargetSocket);
+
+    /*Dummy conections so TLM doesnt complain*/
+    dummy->xInitSocket(router6->xTargetSocket);
+    dummy->yInitSocket(router3->yTargetSocket);
+    dummy->zInitSocket(router5->zTargetSocket);
+
   }   
 };   
    
